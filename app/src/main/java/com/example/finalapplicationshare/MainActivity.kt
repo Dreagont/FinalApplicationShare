710014262c047c0460a95458a183274b99b0979c
Dreagont <huynhannguyen333@gmail.com>
2023-11-21 21:09:07 +0700

    test

--- app/src/main/java/com/example/finalapplicationshare/MainActivity.kt
+++ app/src/main/java/com/example/finalapplicationshare/MainActivity.kt
@@ -13,92 +13,11 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var addUsernameEditText: EditText
-    private lateinit var addPasswordEditText: EditText
-    private lateinit var btnAddTest: Button
-
-    private lateinit var showAddUsernameEditText: TextView
-    private lateinit var showAddPasswordEditText: TextView
-    private lateinit var btnShowTest: Button
-
-    private var firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private var databaseReference: DatabaseReference = firebaseDatabase.reference
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addUsernameEditText = findViewById(R.id.addUsername)
-        addPasswordEditText = findViewById(R.id.addPassword)
-        btnAddTest = findViewById(R.id.btnAddTest)
 
-        showAddUsernameEditText = findViewById(R.id.showAddUsername)
-        showAddPasswordEditText = findViewById(R.id.ShowAddPassword)
-
-        showAddUsernameEditText = findViewById(R.id.showAddUsername)
-        showAddPasswordEditText = findViewById(R.id.ShowAddPassword)
-        btnShowTest = findViewById(R.id.btnShowTest)
-
-
-        firebaseDatabase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDatabase.getReference("Users")
-
-        btnAddTest.setOnClickListener {
-            val username = addUsernameEditText.text.toString()
-            val password  =addPasswordEditText.text.toString()
-            addValue(username, password)
-        }
-
-        btnShowTest.setOnClickListener {
-            val username = addUsernameEditText.text.toString()
-            val password  =addPasswordEditText.text.toString()
-            showValue(username)
-        }
     }
-
-    private fun showValue(username: String) {
-        databaseReference.child(username).addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                        val user = snapshot.getValue(User::class.java)
-                        user?.let {
-                            val name = it.name
-                            val password = it.password
-
-                            if (name != null && password != null) {
-                                showAddUsernameEditText.text = name
-                                showAddPasswordEditText.text = password
-                            }
-                        }
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
-    }
-
-
-    private fun addValue(username: String, password: String) {
-        val user = User(username, password)
-        databaseReference.child(username).setValue(user)
-    }
-
-
-    data class User(
-        val name: String = "",
-        val password: String = ""
-    ) {
-
-        override fun toString(): String {
-            return "User(name='$name', password='$password')"
-        }
-    }
-
-
-
-
 }
 
