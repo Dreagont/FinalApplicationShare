f05bcb4c27e306fac7cc9954da69777451ab0359
midterm <1lytravai@gmail.com>
2023-12-01 13:29:59 +0700

    an ver1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("at.favre.lib:bcrypt:2.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
